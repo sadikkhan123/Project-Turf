6d6f6bad7f686b481fcfecf9333407de7035977d
sadikkhan123 <sadikkhalidkhan@gmail.com>
2023-08-22 10:28:20 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Route,  Routes } from 'react-router-dom'; // Updated import statements
+import Register from "./components/Register"
+import Sign from "./components/Sign"
+import Orders from "./components/Orders"
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import CreateTurf from './components/CreateTurf';
+import MyTurf from './components/MyTurf';
+import BookTurf from './components/BookTurf';
+// import { Home } from '@mui/icons-material';
+import Singleturf from './components/Singleturf';
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+     <Header/>
+        <Routes> 
+          <Route path="/Home" exact element={<Home />} />
+          <Route path="/Orders" exact element={<Orders/>} />
+          <Route path="/About" exact element={<About />} />
+          <Route path="/Contact" exact element={<Contact/>} />
+          <Route path="/CreateTurf" exact element={<CreateTurf/>} />
+          <Route path="/MyTurf" exact element={<MyTurf />} />
+          <Route path="/seeTurf" exact element={<seeTurf />} />
+          <Route path="/BookTurf" element={<BookTurf/>} />
+          <Route path="/" element={<Sign />} />
+          <Route path="/SingleTurf/:id" element={<Singleturf/>} />
+         
+        </Routes>
+      {/* <Footer/> */}
     </div>
   );
 }
 
 export default App;
+
